088d486e7f43a646a02145c51d5c0e9333a19995
Tammi Palmer <Tammi@Jahmels-MacBook-Air.local>
2019-12-01 10:07:09 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,28 +27,45 @@ var cards = [
 var cardsInPlay = [];
 // this will store the cards in play
 
+//.  commented on this to tak out temp document.getElementsByTagName('a')[0].setAttribute(cards[cardId].cardImage)
+// not sure if this will work to set the card image.
+
+
 function checkForMatch(){
  if (cardsInPlay[0] === cardsInPlay[1]) {
    console.log("You found a match!");
  } else {
    console.log("Sorry, try again.");
 }
+//checks to see if the cards match
 
 }
 
-function flipCard(cardId) {
+ var createBoard = function() {
+for (var i = 0; i < cards.length; i++) {
+  	//loop throught the cards
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src',"images/back.png");
+  cardElement.className = 'card-' +1;
+  cardElement.setAttribute('data-id','cardElement' + i);
+  // create a new img element and stores it in card element
+  document.getElementsByName('game-board').appendChild(cardElement);
+}
+};
 
+var button = document.querySelector('button');
+button.addEventListener('click', flipCard);
+
+
+function flipCard() {
+ this.getAttribute('data-id')
  console.log("User flipped " + cards[cardId].rank);
  cardsInPlay.push(cards[cardId].rank);
  console.log(cards[cardId].cardImage);
  console.log(cards[cardId].suit);
+ checkForMatch();
+ }
 
-checkForMatch();
- 
-}
-
-flipCard(0);
-flipCard(2);
-
-
+createBoard();
+document.getElementsByName('src').setAttribute(cardImage);
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");} else {alert("Sorry, try again.");};
